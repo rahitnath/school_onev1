97bcedf9d9dbc2bc64cac889fde546414df142cb
rahit <rahitnath713203@gmail.com>
2023-04-08 18:24:30 +0530

    Adding All New JS Scripts For Login Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-// import logo from './logo.svg';
+import logo from '../src/Resource/unicornBG.png';
 import HCV from './JSFiles/Greet';
 import './App.css';
 
 function App() {
   return (
       <div className="App">
+        {/* <img src={logo}/> */}
         <HCV className="header"></HCV>
+        
       </div>
   );
 }
--- src/JSFiles/Greet.js
+++ src/JSFiles/Greet.js
@@ -1,10 +1,33 @@
 import { Button, colors, CardHeader } from "@mui/material";
-
-const DefaultFunction = () => {
+import React,{ useState } from "react";
+const DefaultFunction = (...args) => {
+  // const value = ...args;
+  const [username, setUsername]=useState();
+  const [isValid, setIsValid]=useState(false);
+  React.useEffect(() => {
+    console.log(username);
+    if(username === "aaa"){
+      setIsValid((prev) => !prev);
+    }
+  }, [username])
   return(
-<div>
-    <CardHeader style={{backgroundColor:"#edd8f9",height:"90px",textAlign: 'center',textShadow:"20px"}} className="Containerheader"   title = "Trip Till Die"> <div></div></CardHeader>
-</div>
+      <div name= "RootContainer" className="rootContainer">
+        <div>
+        <label className="label-username"><span style={{color:"red"}}>Username</span></label>
+        <input className="username" id="Uname" name="username" type="text" onChange={(e) => setUsername(e.target.value)} 
+        style={
+          
+            {color: isValid? 'black':'red',borderWidth:1}
+        
+      }
+        ></input>
+        </div>
+        <div>
+        <label className="label-password"><span style={{color:"red"}}>Password</span></label>
+        <input className="password" id="pass" name="password" type="text"></input>
+        </div>
+        <button type="button" name="submit" className="submit"> SUBMIT</button>
+      </div>
   )
 };
 
